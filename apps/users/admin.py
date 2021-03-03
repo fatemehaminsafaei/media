25380faab44cd214f2ce23f54111d0470d3fc970
fatemeh.as <fatemehaminsafaei@gmail.com>
2021-03-03 17:57:42 +0330

    Initial commit

---
+++ apps/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+# Register your models here.
+from apps.users.models import Profile, FriendRequest
+
+admin.site.register(Profile)
+admin.site.register(FriendRequest)
